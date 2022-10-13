78c5e846ce6d77156717db569653ef875d54c9eb
akarshkh <115661993+akarshkh@users.noreply.github.com>
2022-10-14 00:12:24 +0530

    Add files via upload

---
+++ areaofrect.c
@@ -0,0 +1,15 @@
+//write a c program to enter length and breadth of rectangle and find its area.
+#include<stdio.h>
+int main()
+{
+   int l,b;
+    printf("Enter the value of l in cm\n");
+    scanf("%d",&l);
+    printf("Enter the value of b in cm\n");
+    scanf("%d",&b);
+    l*b;
+    printf("the area of rectangle is %dcm",l*b);
+    
+    return 0;
+
+}
